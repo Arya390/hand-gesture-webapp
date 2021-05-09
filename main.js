03d90751b6e0e0c125136866194a122fb8b8e52a
Arya390 <75195330+Arya390@users.noreply.github.com>
2021-05-09 16:18:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,56 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log('Model Loaded!');
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "the first prediction is" + prediction_1;
+    speak_data_2 = "the second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    utterThis.rate = 0.5;
+    synth.speak(utterThis);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("results_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "best")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "amazing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "claping")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128079;";
+        }
+
+        if(results[1].label == "best")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "amazing")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if(results[1].label == "claping")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128079;";
+        }
+    }
+}
